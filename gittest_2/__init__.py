d98fec204e54efe394b9c3a9f23d22c180de7333
whjing <wh.jing@outlook.com>
2024-04-12 16:30:09 +0800

    gigds

--- gittest_2/__init__.py
+++
@@ -1,9 +0,0 @@
-# !/user/bin/env python
-# -*- coding: utf-8 -*-
-import sys
-import os
-res = "/Users/jing/academic/module"
-#res = os.path.dirname(os.path.dirname(__file__))
-sys.path.append(res)
-print(res)
-print('Module path successfully appended.')
